a309764aaf9e361a0c05fdda471b7bc45fea1173
Jose Alvarado <jos.alvatorre@gmail.com>
2024-09-25 21:40:41 -0700

    lint

--- abacus/genesis/main.tf
+++ abacus/genesis/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  # At time of writing, we simply use the latest version of Terraform available on HCP Terraform.
+  required_version = ">= 1.9.6"
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.68.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-west-1"
 }
