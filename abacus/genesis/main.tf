49da74ee74013792647f1bf5e23f422db307f01a
Jose Alvarado <jos.alvatorre@gmail.com>
2024-09-26 20:53:10 -0700

    put aws in its own module

--- abacus/genesis/main.tf
+++ abacus/genesis/main.tf
@@ -10,42 +10,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-west-1"
-}
-
-import {
-  to = aws_organizations_organization.org
-  id = "o-5hzhyf326b"
-}
-
-resource "aws_organizations_organization" "org" {
-  # https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_org_support-all-features.html
-  feature_set = "ALL"
-  aws_service_access_principals = [
-    "sso.amazonaws.com",
-  ]
-}
-
-import {
-  to = aws_organizations_account.abacus_org
-  id = "339712758060"
-}
-
-# This is the root account for the org.
-resource "aws_organizations_account" "abacus_org" {
-  name      = "abacus-org"
-  email     = "the.abacus.app@gmail.com"
-  role_name = "OrganizationAccountAccessRole"
-}
-
-import {
-  to = aws_organizations_account.abacus_images
-  id = "992382556889"
-}
-
-resource "aws_organizations_account" "abacus_images" {
-  name      = "abacus-images"
-  email     = "the.abacus.app+images@gmail.com"
-  role_name = "OrganizationAccountAccessRole"
+module "aws" {
+  source = "./aws"
 }
