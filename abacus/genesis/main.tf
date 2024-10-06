33e11615a3d4e1cd89fb51450bbd88a1b6f0ddbb
Jose Alvarado <jos.alvatorre@gmail.com>
2024-10-06 15:15:45 -0700

    Add Terraform fmt check for Genesis and add minimal Terraform config (#9)
    This PR adds a format check build step for the Genesis Terraform code.
    
    We also add a "cloud block" to the Genesis Terraform code. This is
    necessary to trigger our CLI operations in our cloud account instead of
    locally.

--- abacus/genesis/main.tf
+++ abacus/genesis/main.tf
@@ -1,51 +1,12 @@
 terraform {
   # At time of writing, we simply use the latest version of Terraform available on HCP Terraform.
-  required_version = ">= 1.9.6"
-  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.68.0"
+  required_version = ">= 1.9.7"
+  # https://developer.hashicorp.com/terraform/language/terraform#terraform-cloud
+  cloud {
+    organization = "abacus_org"
+    workspaces {
+      name    = "genesis"
+      project = "default_project"
     }
   }
-}
-
-provider "aws" {
-  region = "us-west-1"
-}
-
-import {
-  to = aws_organizations_organization.org
-  id = "o-5hzhyf326b"
-}
-
-resource "aws_organizations_organization" "org" {
-  # https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_org_support-all-features.html
-  feature_set = "ALL"
-  aws_service_access_principals = [
-    "sso.amazonaws.com",
-  ]
-}
-
-import {
-  to = aws_organizations_account.abacus_org
-  id = "339712758060"
-}
-
-# This is the root account for the org.
-resource "aws_organizations_account" "abacus_org" {
-  name      = "abacus-org"
-  email     = "the.abacus.app@gmail.com"
-  role_name = "OrganizationAccountAccessRole"
-}
-
-import {
-  to = aws_organizations_account.abacus_images
-  id = "992382556889"
-}
-
-resource "aws_organizations_account" "abacus_images" {
-  name      = "abacus-images"
-  email     = "the.abacus.app+images@gmail.com"
-  role_name = "OrganizationAccountAccessRole"
 }
