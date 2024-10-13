3cc5ad01688e5d831cf59dc6d334da5fcb8bd965
Jose Alvarado <jos.alvatorre@gmail.com>
2024-10-13 16:50:44 -0700

    go back to using variables

--- abacus/genesis/main.tf
+++ abacus/genesis/main.tf
@@ -2,24 +2,22 @@ locals {
   the_abacus_app_email              = "the.abacus.app@gmail.com"
   terraform_cloud_aws_oidc_audience = "terraform-cloud.aws-workload-identity"
   terraform_cloud_hostname          = "app.terraform.io"
-  # TODO replace these with terraform.cloud.* references when we re-add the cloud block below.
-  terraform_cloud_organization = "abacus_org"
-  terraform_cloud_project      = "default_project"
-  terraform_cloud_workspace    = "genesis"
+  terraform_cloud_organization      = "abacus_org"
+  terraform_cloud_project           = "default_project"
+  terraform_cloud_workspace         = "genesis"
 }
 
 terraform {
   # At time of writing, we simply use the latest version of Terraform available on HCP Terraform.
   required_version = ">= 1.9.7"
-  # TODO Add this back in when AWS auth is set up.
   # https://developer.hashicorp.com/terraform/language/terraform#terraform-cloud
-  # cloud {
-  #   organization = "abacus_org"
-  #   workspaces {
-  #     name    = "genesis"
-  #     project = "default_project"
-  #   }
-  # }
+  cloud {
+    organization = "abacus_org"
+    workspaces {
+      name    = "genesis"
+      project = "default_project"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
